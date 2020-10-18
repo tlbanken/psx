02b976b14a30a4e243cc9d44da0c82c081c37964
tlbanken <bankenlt63@gmail.com>
2020-10-18 15:28:18 -0700

    Init main file

---
+++ src/psx.cpp
@@ -0,0 +1,16 @@
+/*
+ * psx.cpp
+ * 
+ * Travis Banken
+ * 2020
+ * 
+ * Main file for the PSX project.
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, PSX!\n";
+    return 0;
+}
